75eec1cc0e560e152e2fe18eafb2ed2762003800
mo-g-19 <sggibson619@gmail.com>
2025-09-12 16:25:22 -0700

    I forgot to comment out the output. Need to have networkx library downloaded

--- testingCreatingGML.py
+++ testingCreatingGML.py
@@ -1,18 +1,19 @@
 #Copying the example from https://networkx.org/documentation/stable/reference/readwrite/generated/networkx.readwrite.gml.generate_gml.html#networkx.readwrite.gml.generate_gml
 #to see what happens
+import networkx as nx
 
 G = nx.Graph()
 G.add_node("1")
 print("\n".join(nx.generate_gml(G)))
-graph [
+"""graph [
   node [
     id 0
     label "1"
   ]
-]
+]"""
 G = nx.MultiGraph([("a", "b"), ("a", "b")])
 print("\n".join(nx.generate_gml(G)))
-graph [
+"""graph [
   multigraph 1
   node [
     id 0
@@ -32,4 +33,4 @@ graph [
     target 1
     key 1
   ]
-]
+]"""
