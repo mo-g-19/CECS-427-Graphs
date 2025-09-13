10855cd944e64aedd27cc16b6a5ad3b8c3f14442
mo-g-19 <sggibson619@gmail.com>
2025-09-13 11:10:57 -0700

    Wrote out partial sudo code from CECS 328 for BFS. Now looking back at partner's code to see how it fits

---
+++ create_random_graph.py
@@ -0,0 +1,117 @@
+import time
+import math
+import argparse
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+# Functions
+# ====================================================================================================
+def load_gml(path: str):
+    print(f"load_gml called with path={path}")
+    return "PLACEHOLDER"
+
+def save_gml(G, path: str):
+    nx.write_gml(G, path)
+    print(f"Graph saved to {path}")
+
+
+def create_random_graph(n: int, c: float):
+    seed = int(time.time())
+    p = (c * math.log(n)) / n                   #calculating the probablility based off the probablilty funtion in the assignment
+    G = nx.erdos_renyi_graph(n, p, seed=seed)    #generating the graph
+    return  G
+
+def multi_BFS(G, start_nodes: list[int]):
+    print(f"multi_BFS called on G with start_nodes={start_nodes}")
+
+
+def analyze_graph(G):
+    print("analyze_graph called on G")
+
+
+def plot_graph(G):
+    seed = int(time.time())
+    pos = nx.spring_layout(G, seed=seed)    #Honesly dont know what this does just copied this from the test code 
+    nx.draw(G, pos=pos)                     #place holder anyway this whole funtion needs to be redone
+    plt.show()
+
+
+
+# Arg Parser
+# ====================================================================================================
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Graph CLI")
+
+    parser.add_argument(
+        "--input",
+        type=str,
+        help="Path to input GML file"
+        )
+    parser.add_argument(
+        "--output", 
+        type=str,
+        help="Path to output GML file"
+        )
+
+    parser.add_argument(
+        "--create_random_graph",
+        nargs=2,
+        metavar=("n", "c"),
+        help="Generate random graph with n nodes and parameter c",
+    )
+
+    parser.add_argument(
+        "--multi_BFS",
+        nargs="+",
+        type=int,
+        metavar="start_nodes",
+        help="Run multi-source BFS from given nodes",
+    )
+
+    parser.add_argument(
+        "--analyze",
+        action="store_true",
+        help="Print graph analysis",
+    )
+
+    parser.add_argument(
+        "--plot",
+        action="store_true",
+        help="Show graph plot",
+    )
+
+    return parser
+
+
+
+# Main
+# ====================================================================================================
+def main():
+    parser = build_parser()
+    args = parser.parse_args()
+
+    G = None   # local graph variable
+
+    if args.input:
+        G = load_gml(args.input)
+
+    if args.create_random_graph:
+        n = int(args.create_random_graph[0])
+        c = float(args.create_random_graph[1])
+        G = create_random_graph(n, c)
+
+    if args.multi_BFS and G:
+        multi_BFS(G, args.multi_BFS)
+
+    if args.analyze and G:
+        analyze_graph(G)
+
+    if args.plot and G:
+        plot_graph(G)
+
+    if args.output and G:
+        save_gml(G, args.output)
+
+if __name__ == "__main__":
+    main()
--- firstTrial.py
+++ firstTrial.py
@@ -1,5 +1,5 @@
 #Following code lines 2-7 from video https://www.youtube.com/watch?time_continue=37&v=7OdXry0T9Vg&embeds_referring_euri=https%3A%2F%2Fvideo.search.yahoo.com%2Fsearch%2Fvideo%3B_ylt%3DAwrOsH5opMRoPy0Ek0JXNyoA%3B_ylu%3DY29sbwNncTEEcG9zAzIEdnRpZAMEc2VjA3Nj%3Ftype%3DE210US1&embeds_referring_origin=https%3A%2F%2Fvideo.search.yahoo.com&source_ve_path=MzY4NDIsMzY4NDIsMjM4NTE
-import xml.etree.ElementTree as etree
+"""import xml.etree.ElementTree as etree
 
 #Load the GML file
 data = et.parse("HT.gml")
@@ -21,4 +21,26 @@ result = []
 for el in root.findall(xpath_query):
     result.append(el.text)
 
-print(result)       #This outputs the coordinates specifically from Building elements
+print(result)       #This outputs the coordinates specifically from Building elements"""
+
+import time
+import argparse
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def multi_BFS(G, start_nodes: list[int]):
+    print(f"multi_BFS called on G with start_nodes={start_nodes}")
+
+#Using the notes from CECS 328 when created a BFS
+def general_BFS(G, initial_node){
+    visited = []
+    queue = []
+    queue.append(initial_node)
+    while queue is not empty:
+        x = queue.pop()
+        if (x is not in visited):
+            visited.append(x)
+        for every edge (x, y):
+            if y is not in visited:
+                queue.append(y)
+}
