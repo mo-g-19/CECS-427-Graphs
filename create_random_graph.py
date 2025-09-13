d687059cef20a00d31b4f672fbd71fec5aa6d822
mo-g-19 <sggibson619@gmail.com>
2025-09-13 11:51:58 -0700

    Okay. I think I have an idea of how to approach this using my notes from CECS 328

--- create_random_graph.py
+++ create_random_graph.py
@@ -5,26 +5,50 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 
-# Functions
-# ====================================================================================================
-def load_gml(path: str):
-    print(f"load_gml called with path={path}")
-    return "PLACEHOLDER"
-
-def save_gml(G, path: str):
-    nx.write_gml(G, path)
-    print(f"Graph saved to {path}")
-
-
 def create_random_graph(n: int, c: float):
     seed = int(time.time())
     p = (c * math.log(n)) / n                   #calculating the probablility based off the probablilty funtion in the assignment
     G = nx.erdos_renyi_graph(n, p, seed=seed)    #generating the graph
     return  G
 
+def save_gml(G, path: str):
+    nx.write_gml(G, path)
+    print(f"Graph saved to {path}")
+
+
 def multi_BFS(G, start_nodes: list[int]):
     print(f"multi_BFS called on G with start_nodes={start_nodes}")
 
+#Using the notes from CECS 328 when created a BFS
+def general_BFS(G, initial_node){
+    final = []
+    queue = []
+    queue.append(initial_node)
+    while queue is not empty:
+        x = queue.pop()
+        if (x is not in visited):
+            visited.append(x)
+        for every edge (x, y):
+            if y is not in visited:
+                queue.append(y)
+}
+
+# Main
+# ====================================================================================================
+def main():
+    nodes = 20
+    edge_creation = 0.5
+
+    Graph = create_random_graph(nodes, edge_creation)
+    Graph.add_nodes_from(list(n for n in Graph.nodes), visited=0)       #Instead of creating a visited array (which could make it O(n) if constantly searching through see if visited -> O(1) and an attribute) 0 -> not visited; 1 -> visited
+    
+    save_gml(Graph, "mo_analyze_graph.gml")
+
+if __name__ == "__main__":
+    main()
+
+
+""" Next on agenda
 
 def analyze_graph(G):
     print("analyze_graph called on G")
@@ -36,82 +60,4 @@ def plot_graph(G):
     nx.draw(G, pos=pos)                     #place holder anyway this whole funtion needs to be redone
     plt.show()
 
-
-
-# Arg Parser
-# ====================================================================================================
-def build_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(description="Graph CLI")
-
-    parser.add_argument(
-        "--input",
-        type=str,
-        help="Path to input GML file"
-        )
-    parser.add_argument(
-        "--output", 
-        type=str,
-        help="Path to output GML file"
-        )
-
-    parser.add_argument(
-        "--create_random_graph",
-        nargs=2,
-        metavar=("n", "c"),
-        help="Generate random graph with n nodes and parameter c",
-    )
-
-    parser.add_argument(
-        "--multi_BFS",
-        nargs="+",
-        type=int,
-        metavar="start_nodes",
-        help="Run multi-source BFS from given nodes",
-    )
-
-    parser.add_argument(
-        "--analyze",
-        action="store_true",
-        help="Print graph analysis",
-    )
-
-    parser.add_argument(
-        "--plot",
-        action="store_true",
-        help="Show graph plot",
-    )
-
-    return parser
-
-
-
-# Main
-# ====================================================================================================
-def main():
-    parser = build_parser()
-    args = parser.parse_args()
-
-    G = None   # local graph variable
-
-    if args.input:
-        G = load_gml(args.input)
-
-    if args.create_random_graph:
-        n = int(args.create_random_graph[0])
-        c = float(args.create_random_graph[1])
-        G = create_random_graph(n, c)
-
-    if args.multi_BFS and G:
-        multi_BFS(G, args.multi_BFS)
-
-    if args.analyze and G:
-        analyze_graph(G)
-
-    if args.plot and G:
-        plot_graph(G)
-
-    if args.output and G:
-        save_gml(G, args.output)
-
-if __name__ == "__main__":
-    main()
+"""
---
+++ mo_analyze_graph.gml
@@ -0,0 +1,150 @@
+graph [
+  node [
+    id 0
+    label "0"
+    visited 20
+  ]
+  node [
+    id 1
+    label "1"
+    visited 20
+  ]
+  node [
+    id 2
+    label "2"
+    visited 20
+  ]
+  node [
+    id 3
+    label "3"
+    visited 20
+  ]
+  node [
+    id 4
+    label "4"
+    visited 20
+  ]
+  node [
+    id 5
+    label "5"
+    visited 20
+  ]
+  node [
+    id 6
+    label "6"
+    visited 20
+  ]
+  node [
+    id 7
+    label "7"
+    visited 20
+  ]
+  node [
+    id 8
+    label "8"
+    visited 20
+  ]
+  node [
+    id 9
+    label "9"
+    visited 20
+  ]
+  node [
+    id 10
+    label "10"
+    visited 20
+  ]
+  node [
+    id 11
+    label "11"
+    visited 20
+  ]
+  node [
+    id 12
+    label "12"
+    visited 20
+  ]
+  node [
+    id 13
+    label "13"
+    visited 20
+  ]
+  node [
+    id 14
+    label "14"
+    visited 20
+  ]
+  node [
+    id 15
+    label "15"
+    visited 20
+  ]
+  node [
+    id 16
+    label "16"
+    visited 20
+  ]
+  node [
+    id 17
+    label "17"
+    visited 20
+  ]
+  node [
+    id 18
+    label "18"
+    visited 20
+  ]
+  node [
+    id 19
+    label "19"
+    visited 20
+  ]
+  edge [
+    source 0
+    target 5
+  ]
+  edge [
+    source 1
+    target 8
+  ]
+  edge [
+    source 1
+    target 10
+  ]
+  edge [
+    source 2
+    target 4
+  ]
+  edge [
+    source 6
+    target 12
+  ]
+  edge [
+    source 6
+    target 19
+  ]
+  edge [
+    source 7
+    target 13
+  ]
+  edge [
+    source 8
+    target 19
+  ]
+  edge [
+    source 10
+    target 13
+  ]
+  edge [
+    source 11
+    target 13
+  ]
+  edge [
+    source 12
+    target 19
+  ]
+  edge [
+    source 15
+    target 17
+  ]
+]
