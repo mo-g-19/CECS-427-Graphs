f67a91817c2e940718512ab2ae31f3ae8b09f942
mo-g-19 <sggibson619@gmail.com>
2025-09-13 18:29:02 -0700

    I think I completed the BFS function. Putting it in graph.py without all the print statements. Keeping the print statements in create_random_graph, so I can double check later if needed

--- create_random_graph.py
+++ create_random_graph.py
@@ -1,6 +1,7 @@
 import time
 import math
 import argparse
+import numpy as np        #One I added
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -18,12 +19,65 @@ def save_gml(G, path: str):
 
 def multi_BFS(G, start_nodes: list[int]):
     print(f"multi_BFS called on G with start_nodes={start_nodes}")
+    print()
+
+    print(f"start_nodes len: {len(start_nodes)}")
+    print(f"number of nodes: {G.number_of_nodes()}")
+    print()
+
+
+    final_BFS = []
+    #np.zeros((len(start_nodes), (G.number_of_nodes())))
+    #counter_BFS = 0
+
+    G.add_nodes_from(list(n for n in G.nodes), visited=False)       #Instead of creating a visited array (which could make it O(n) if constantly searching through see if visited -> O(1) and an attribute) 0 -> not visited; 1 -> visited
+    
+    for node in start_nodes:
+        final = []
+        queue = []
+
+        queue.append(node)
+
+        while len(queue) > 0:
+            print(f"queue: {queue}")
+            x = queue.pop()
+            print(f"current node: {x}")
+
+            if not G.nodes[x]['visited']:
+                print(f"current node visited: {G.nodes[x]['visited']}")
+                G.nodes[x]['visited'] = True
+                print(f"current node visited: {G.nodes[x]['visited']}")
+                print()
+                #nx.set_node_attributes(G, x, visited)
+
+            for edge in G.edges(x):             #edge in (G.in_edges(x) + G.out_edges(x)):
+            #every edge (x, y):
+                print(f"edge for current node {x}: {edge[1]}")
+                if not G.nodes[edge[1]]['visited']:
+                    queue.append(edge[1])
+            
+            print()
+            final.append(x)
+            print(f"final: {final}")
+            print()
+
+        print(f"BFS for {node}: {final}")
+        print()
+        
+        final_BFS.append(final)
+        print(f"final_BFS: {final_BFS}")
+
+        for visited_node in G:
+            G.nodes[visited_node]['visited'] = False
+
+    return final_BFS
+    
 
 #Using the notes from CECS 328 when created a BFS
-def general_BFS(G, initial_node_id):
+def general_BFS(G, initial_node_id: int):
     final = []
     queue = []
-    queue.append(int(initial_node_id))
+    queue.append(initial_node_id)
     while len(queue) > 0:
         print(f"queue: {queue}")
         x = queue.pop()
@@ -51,13 +105,18 @@ def main():
     edge_creation = 0.5
 
     Graph = create_random_graph(nodes, edge_creation)
-    Graph.add_nodes_from(list(n for n in Graph.nodes), visited=False)       #Instead of creating a visited array (which could make it O(n) if constantly searching through see if visited -> O(1) and an attribute) 0 -> not visited; 1 -> visited
+    
     #nx.set_node_attributes(Graph, False, "visited")     #https://stackoverflow.com/questions/54497929/networkx-setting-node-attributes-from-dataframe
 
     save_gml(Graph, "mo_analyze_graph.gml")
 
-    potential_q = general_BFS(Graph, "1")
-    print(potential_q)
+    potential_q = multi_BFS(Graph, [1, 2])
+    for indv_BFS in potential_q:
+        print(indv_BFS)
+    #print(potential_q)
+
+    """for list_order in potential_q[0].len():
+        print(potential_q[-list_order])"""
 
 if __name__ == "__main__":
     main()
--- mo_analyze_graph.gml
+++ mo_analyze_graph.gml
@@ -2,114 +2,82 @@ graph [
   node [
     id 0
     label "0"
-    visited 0
   ]
   node [
     id 1
     label "1"
-    visited 0
   ]
   node [
     id 2
     label "2"
-    visited 0
   ]
   node [
     id 3
     label "3"
-    visited 0
   ]
   node [
     id 4
     label "4"
-    visited 0
   ]
   node [
     id 5
     label "5"
-    visited 0
   ]
   node [
     id 6
     label "6"
-    visited 0
   ]
   node [
     id 7
     label "7"
-    visited 0
   ]
   node [
     id 8
     label "8"
-    visited 0
   ]
   node [
     id 9
     label "9"
-    visited 0
   ]
   node [
     id 10
     label "10"
-    visited 0
   ]
   node [
     id 11
     label "11"
-    visited 0
   ]
   node [
     id 12
     label "12"
-    visited 0
   ]
   node [
     id 13
     label "13"
-    visited 0
   ]
   node [
     id 14
     label "14"
-    visited 0
   ]
   node [
     id 15
     label "15"
-    visited 0
   ]
   node [
     id 16
     label "16"
-    visited 0
   ]
   node [
     id 17
     label "17"
-    visited 0
   ]
   node [
     id 18
     label "18"
-    visited 0
   ]
   node [
     id 19
     label "19"
-    visited 0
-  ]
-  edge [
-    source 0
-    target 4
-  ]
-  edge [
-    source 0
-    target 14
-  ]
-  edge [
-    source 1
-    target 10
   ]
   edge [
     source 1
@@ -117,42 +85,58 @@ graph [
   ]
   edge [
     source 1
-    target 15
+    target 19
   ]
   edge [
     source 2
-    target 3
+    target 7
   ]
   edge [
     source 2
-    target 10
+    target 14
   ]
   edge [
     source 3
-    target 19
+    target 17
   ]
   edge [
     source 4
-    target 5
+    target 11
   ]
   edge [
-    source 6
-    target 12
-  ]
-  edge [
-    source 6
+    source 4
     target 19
+  ]
+  edge [
+    source 6
+    target 11
   ]
   edge [
     source 7
+    target 13
+  ]
+  edge [
+    source 7
+    target 16
+  ]
+  edge [
+    source 8
     target 19
+  ]
+  edge [
+    source 9
+    target 16
   ]
   edge [
     source 10
     target 17
   ]
   edge [
-    source 16
-    target 17
+    source 12
+    target 19
+  ]
+  edge [
+    source 13
+    target 16
   ]
 ]
